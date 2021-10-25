b51a0e4aa4f0b90c1d0a11f3218cd853f2b7fd0c
Oladimeji Mudele <dimeji@Oladimejis-MBP.wind3.hub>
2021-10-26 01:18:44 +0200

    Refactored _get_method fn in Emissivity class

--- emissivity/emissivity.py
+++ emissivity/emissivity.py
@@ -4,31 +4,41 @@ from emissivity.methods import ComputeMonoWindowEmissivity, ComputeEmissivityNBE
 
 
 
-EMISSIVITY_METHODS = ['avdan', 'xiaolei', 'gopinadh']
+#EMISSIVITY_METHODS = ['avdan', 'xiaolei', 'gopinadh']
+
+EMISSIVITY_METHODS = {'avdan': ComputeMonoWindowEmissivity, 
+                        'xiaolei': ComputeEmissivityNBEM, 
+                        'gopinadh': ComputeEmissivityGopinadh
+                        }
 
 class Emissivity:
 
-    def __init__(self, ndvi, red_band=None):
+    def __init__(self, ndvi, red_band=None, methods=EMISSIVITY_METHODS):
+        """
+        This class computes the land surface emissivity and returns it as a numpy array
+
+        Args:
+            ndvi (np.ndarray[float]): Normalized difference vegetation index (NDVI) image matrix
+            red_band (np.ndarray[float]): Red band of image (0.63-0.69 micrometers)
+        """
         self.ndvi = ndvi
         self.red_band = red_band
+        self.methods = methods
     
     def __call__(self, method):
         assert method in EMISSIVITY_METHODS, ValueError("Method not implemented")
-        compute_method = self.get_method(method)
+        compute_method = self._get_method(method)
 
         return compute_method(self.ndvi, self.red_band)()
 
 
-    def get_method(self, method):
+    def _get_method(self, method):
+        
+        callable_ = self.methods.get(method)
+        if not callable_:
+            raise NotImplementedError("Requested method not implemented. Choose among available methods: {self.methods}")
 
-        if method == 'avdan':
-            return ComputeMonoWindowEmissivity
-        elif method == 'xiaolei':
-            return ComputeEmissivityNBEM
-        elif method == 'gopinadh':
-            return ComputeEmissivityGopinadh
-        else:
-            raise NotImplementedError("Requested method not implemented. Choose among available methods: {EMMISIVITY_METHODS}")
+        return callable_
 
         
 
