d99959aa278f2d17074dff873f6f39301a9b2572
Oladimeji Mudele <mudeledimeji@gmail.com>
2021-12-04 05:10:00 +0100

    cleaned __init__.py of temperature

--- pylandtemp/temperature/__init__.py
+++ pylandtemp/temperature/__init__.py
@@ -1,6 +1,2 @@
-# from pylandtemp.temperature.brightness_temperature import BrightnessTemperatureLandsat
-
-# from pylandtemp.temperature.temperature_main import LST, SINGLE_WINDOW_METHODS, SPLIT_WINDOW_METHODS
-
 from .temperature import default_algorithms
 from .brightness_temperature import BrightnessTemperatureLandsat
